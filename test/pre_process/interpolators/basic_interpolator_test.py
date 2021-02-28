d51ef18ee0407d6644aea2d5dfff15b1c947fc6b
ELIAV <eliav9896@gmail.com>
2021-02-28 13:22:10 +0200

    Shelving class for committing past work

---
+++ test/pre_process/interpolators/basic_interpolator_test.py
@@ -0,0 +1,14 @@
+import unittest
+import datetime
+from typing import List
+
+
+def generate_datetime(size) -> List[datetime.datetime]:
+
+
+class BasicInterpolatorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.stocks_data = {'temp_stock_1' : []}
+
+    def test(self):
